14b4f63308cc41bcb2d7b7e8c4fbcaf881904e35
GabrielLima1995 <gabriel-souzalima@live.com>
2022-07-26 15:28:40 -0300

    added hop param on preparation.py

---
+++ .ipynb_checkpoints/operations-checkpoint.py
@@ -0,0 +1,18 @@
+from sklearn.preprocessing import MinMaxScaler
+
+def normalize_transform(tensor,sc):
+    
+  for i in range(tensor.shape[1]):
+    tensor[:, i, :] = sc[i].transform(tensor[:, i, :]) 
+  return tensor
+
+
+def normalize_fit(tensor):
+  
+  scalers = {}
+  for i in range(tensor.shape[1]):
+    scalers[i] = MinMaxScaler()
+    tensor[:, i, :] = scalers[i].fit_transform(tensor[:, i, :]) 
+
+  return tensor,scalers
+
